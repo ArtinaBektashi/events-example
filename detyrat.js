bc60727315f75200beb921322c14520e9319c3d4
ArtinaBektashi <ab51034@ubt-uni.net>
2023-02-23 19:16:01 +0100

    promises changed

--- detyrat.js
+++ detyrat.js
@@ -1,38 +1,34 @@
 const fs=require('fs');
 
 
-
-
-function writeFile(path,data){
-    return new Promise((resolve,reject) => {
-        fs.writeFile(path,data, (err)=> {
-            if(err){
-                reject(err);
-            }else{
-                resolve('Written');
-            }
-        })
+const promise = new Promise((resolve,reject) =>{
+    fs.writeFile('file.txt','Hello', (err) =>{
+        if(err){
+            reject(err);
+        }else
+            resolve();
+        
     })
-}
-
-function readFile(path){
-    return new Promise((resolve,reject) => {
-        fs.readFileSync(path,'utf8', (data,err) => {
-            if(err){
-                reject(err);
-            }else{
-                resolve(data);
-            }
-        })
-    })
-}
-
-writeFile('file.txt','Hello World').then((result) =>{
-    console.log(result);
-    return readFile('file.txt');
-}).then((data) =>{
-    console.log(data);
-}).catch(err =>{
-    console.log(err);
 })
 
+const promise2=new Promise((resolve,reject) =>{
+    fs.readFile('file.txt','utf8',(err,data) => {
+        if(err){
+            reject(err);
+        }else{
+            resolve(data);
+        }
+    })
+})
+
+promise.then(() =>{
+    console.log('File written');
+}).catch(()=>{
+    console.log("Error")
+})
+
+promise2.then((data)=>{
+    console.log(data);
+}).catch((err)=>{
+    console.log(err);
+})
